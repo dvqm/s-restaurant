84c1d24ab98ca560148bd5d38f4e81ad2d914c9e
dvqm <10248779+dvqm@users.noreply.github.com>
2022-05-29 15:30:53 +0200

    UICreator. Moved nodeCreate() to the separate method from render().

--- src/UiCreator.js
+++ src/UiCreator.js
@@ -1,69 +1,60 @@
 import contentHTML from './assets/json/contentHTML.json';
 import ContentCreator from './ContentCreator';
 
-
 class UiCreator {
-  constructor(pointer, data) {
-    this.pointer = pointer;
+  constructor() {}
 
-    this.data = data;
+  nodeCreate(data) {
+    const details = structuredClone(data);
+
+    const { tag } = details.d;
+
+    delete details.d.tag;
+
+    const parent = document.createElement(tag);
+
+    Object.entries(details.d).forEach((prop) => {
+      const [key, value] = prop;
+
+      if (key === 'src') {
+        const link = `${value}`;
+
+        parent[key] = require(`./${link}`);
+      } else parent[key] = value;
+    });
+
+    if (details.c) {
+      const { c } = details;
+
+      Object.entries(c).forEach((node) => {
+        const [key, value] = node;
+        if (key === 'i') {
+          delete c[key];
+
+          Object.values(value).forEach((func) => {
+            const content = new ContentCreator(contentHTML);
+
+            const nodes = content[func]();
+
+            Object.values(nodes).forEach((element) => {
+              const child = this.nodeCreate(element);
+
+              parent.append(child);
+            });
+          });
+        } else {
+          const child = this.nodeCreate(value);
+
+          parent.append(child);
+        }
+      });
+    }
+
+    return parent;
   }
 
-  render() {
-    const { data } = this;
-
-    const nodeCreate = (props) => {
-      const details = structuredClone(props);
-
-      const { tag } = details.d;
-
-      delete details.d.tag;
-
-      const parent = document.createElement(tag);
-
-      Object.entries(details.d).forEach((prop) => {
-        const [key, value] = prop;
-
-        if (key === 'src') {
-          const link = `${value}`;
-
-          parent[key] = require(`./${link}`);
-        } else parent[key] = value;
-      });
-
-      if (details.c) {
-        const { c } = details;
-
-        Object.entries(c).forEach((node) => {
-          const [key, value] = node;
-          if (key === 'i') {
-            delete c[key];
-
-            Object.values(value).forEach((func) => {
-              const content = new ContentCreator(contentHTML);
-
-              const nodes = content[func]();
-
-              Object.values(nodes).forEach((element) => {
-                const child = nodeCreate(element);
-
-                parent.append(child);
-              });
-            });
-          } else {
-            const child = nodeCreate(value);
-
-            parent.append(child);
-          }
-        });
-      }
-
-      return parent;
-    };
-
-    const parent = nodeCreate(data);
-
-    this.pointer.append(parent);
+  render(pointer, node) {
+    pointer.append(node);
   }
 }
 
