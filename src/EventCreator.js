f61bb824faf3c1b1639a6fe7352f18cd988cc746
dvqm <10248779+dvqm@users.noreply.github.com>
2022-05-29 15:28:07 +0200

    EventCreator.js created. EventCreator.mainPage() sliders added. Other btns not included.

---
+++ src/EventCreator.js
@@ -0,0 +1,79 @@
+import names from './assets/json/names.json';
+
+class EventCreator {
+  mainPage(node) {
+    const slidesNum = (num, items, cover) => {
+      const { slider } = items;
+
+      const wrapper = cover.querySelector(`.${slider.wrapper}`);
+
+      const content = cover.querySelectorAll(`.${slider.item}`);
+
+      const contentCopy = content;
+
+      const prevBtn = cover.querySelector(`#${slider.prevBtn}`);
+
+      const nextBtn = cover.querySelector(`#${slider.nextBtn}`);
+
+      content.forEach((item) => item.remove());
+
+      for (let i = 0; i < num; i += 1) {
+        wrapper.append(contentCopy[i]);
+      }
+
+      let i = num;
+
+      let j = 0;
+
+      nextBtn.addEventListener('click', () => {
+        if (i === 0 || i === contentCopy.length) i = 0;
+
+        if (j === contentCopy.length) j = 0;
+
+        wrapper.firstChild.remove();
+
+        wrapper.append(contentCopy[i]);
+
+        i += 1;
+
+        j += 1;
+
+        prevBtn.removeAttribute('disabled');
+      });
+
+      prevBtn.addEventListener('click', () => {
+        if (j === 0 || j === contentCopy.length) j = contentCopy.length;
+
+        if (i === 0) i = contentCopy.length;
+
+        wrapper.lastChild.remove();
+
+        i -= 1;
+
+        j -= 1;
+
+        wrapper.prepend(contentCopy[j]);
+
+        nextBtn.removeAttribute('disabled');
+      });
+    };
+
+    const { popularItems } = names;
+
+    const mealSlider = node
+      .querySelector(`.${popularItems.section}`)
+      .querySelector(`.${popularItems.wrapper}`);
+
+    slidesNum(5, popularItems, mealSlider);
+
+    const { menu } = names;
+
+    const menuSlider = node.querySelector(`.${menu.section}`);
+
+    slidesNum(5, menu, menuSlider);
+
+    return node;
+  }
+}
+
+export default EventCreator;
