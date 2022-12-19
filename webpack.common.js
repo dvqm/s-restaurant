d7752f0c275672f7a4ece3156255b15a2fc4404d
dvqm <10248779+dvqm@users.noreply.github.com>
2022-12-18 17:15:48 -0700

    Webpack.config reconfiguration to common, dev and prod package.json updated to newer versions added webpack-merge. json5 and webpackHtmlPlugin moved to the dependencies from dev-dependencies

---
+++ webpack.common.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const json5 = require('json5');
+
+const minifier = {
+  removeComments: true,
+  collapseWhitespace: true,
+  removeRedundantAttributes: true,
+  useShortDoctype: true,
+  removeEmptyAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  keepClosingSlash: true,
+  minifyJS: true,
+  minifyCSS: true,
+  minifyURLs: true,
+};
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    restaurants: './src/rest.js',
+    meals: './src/meals.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/assets/templates/index.html',
+      title: 'Order your meal!',
+      filename: 'index.html',
+      chunks: ['index', 'vendors'],
+      minify: minifier,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/assets/templates/rest.html',
+      title: 'All restaurants',
+      filename: 'rest.html',
+      chunks: ['rest', 'vendors'],
+      minify: minifier,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/assets/templates/meals.html',
+      title: 'All meals',
+      filename: 'meals.html',
+      chunks: ['meals', 'vendors'],
+      minify: minifier,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ],
+  },
+};
