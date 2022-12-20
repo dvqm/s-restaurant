a7361eb4fd537371ca197ba0e6d96071a20a3c3c
dvqm <10248779+dvqm@users.noreply.github.com>
2022-12-20 14:22:04 -0700

    contentCreator refactored: promoCards() popularItems() restContent() menuContent() footerCities() footerMenu() footerNetworks()

--- src/ContentCreator.js
+++ src/ContentCreator.js
@@ -1,309 +1,276 @@
-class ContentCreator {
+import UiCreator from './UiCreator';
+
+class ContentCreator extends UiCreator {
   constructor(settings) {
+    super();
+
     this.settings = settings;
   }
 
   promoCards() {
-    const cards = {};
+    const data = this.settings.promo;
 
-    const { data } = this.settings.promo;
+    const cards = {
+      tag: 'div',
+      className: 'promo',
+      c: data.map(({ img, discount, name, time }) => ({
+        tag: 'div',
+        className: 'dealCard',
+        c: {
+          photoWrap: {
+            tag: 'div',
+            className: 'foodPhoto',
+            c: {
+              foodPhoto: {
+                tag: 'img',
+                src: img,
+                alt: 'Meal picture',
+              },
+              discount: {
+                tag: 'span',
+                className: 'discount',
+                textContent: `${discount} %`,
+              },
+            },
+          },
+          mealName: {
+            tag: 'span',
+            className: 'mealName',
+            textContent: name,
+          },
+          timer: {
+            tag: 'span',
+            className: 'timer',
+            textContent: `${time} Days Remaining`,
+          },
+        },
+      })),
+    };
 
-    Object.entries(data).map((card) => {
-      const [key, prop] = card;
-
-      const { template } = structuredClone(this.settings.promo);
-
-      const photoWrap = template.c.photoWrap.c;
-
-      const foodPhoto = photoWrap.foodPhoto.d;
-
-      foodPhoto.src = prop.img;
-
-      const discount = photoWrap.discount.d;
-
-      discount.textContent = prop.discount
-        .toString()
-        .concat(discount.textContent);
-
-      const mealName = template.c.mealName.d;
-
-      mealName.textContent = prop.name;
-
-      const timer = template.c.timer.d;
-
-      timer.textContent = prop.time.toString().concat(timer.textContent);
-
-      cards[key] = template;
-      return cards[key];
-    });
-    return cards;
+    return this.constructor.node(cards);
   }
 
   popularItems() {
-    const { popularItems } = this.settings;
+    const data = this.settings.popularItems;
 
-    const { sliderContent } = structuredClone(
-      popularItems.template.mealSlider.c
-    );
+    const cards = {
+      tag: 'div',
+      className: 'mealSlider',
+      c: [
+        {
+          tag: 'button',
+          className: 'sliderBtn backward',
+          id: 'prevMealBtn',
+        },
+        {
+          tag: 'div',
+          className: 'sliderContent',
+          c: data.map(({ img, name, restaurant, price, composition }) => ({
+            tag: 'div',
+            className: 'mealCard',
+            c: {
+              mealPhoto: {
+                tag: 'img',
+                className: 'mealPhoto',
+                alt: 'Meal picture',
+                src: `assets/meals/${img}`,
+              },
+              mealName: {
+                tag: 'span',
+                className: 'mealName',
+                textContent: name,
+              },
+              restName: {
+                tag: 'span',
+                className: 'restName',
+                textContent: restaurant,
+              },
+              mealPrice: {
+                tag: 'span',
+                className: 'mealPrice',
+                textContent: `$${price}`,
+              },
+              orderBtn: {
+                tag: 'button',
+                id: 'orderNow',
+                textContent: 'Order Now',
+              },
+              composition: {
+                tag: 'p',
+                className: 'composition',
+                textContent: composition,
+              },
+            },
+          })),
+        },
+        {
+          tag: 'button',
+          className: 'sliderBtn forward',
+          id: 'nextMealBtn',
+        },
+      ],
+    };
 
-    const cards = popularItems.template.mealSlider;
-
-    cards.c.sliderContent.c = {};
-
-    const { data } = popularItems;
-
-    Object.entries(data).map((card) => {
-      const [key, value] = card;
-
-      const template = structuredClone(sliderContent.c.mealCard);
-
-      const mealPhoto = template.c.mealPhoto.d;
-
-      mealPhoto.src = mealPhoto.src.concat(value.img);
-
-      const mealName = template.c.mealName.d;
-
-      mealName.textContent = value.name;
-
-      const restName = template.c.restName.d;
-
-      restName.textContent = value.restaurant;
-
-      const mealPrice = template.c.mealPrice.d;
-
-      mealPrice.textContent = mealPrice.textContent.concat(
-        value.price.toString()
-      );
-
-      const composition = template.c.composition.d;
-
-      composition.textContent = value.composition;
-
-      cards.c.sliderContent.c[key] = template;
-
-      return cards.c.sliderContent.c[key];
-    });
-
-    const shell = {};
-
-    shell.popularItems = cards;
-
-    return shell;
+    return this.constructor.node(cards);
   }
 
   restContent() {
-    const { featuredRestaurants } = this.settings;
+    const data = this.settings.featuredRestaurants;
 
-    const restContent = structuredClone(
-      featuredRestaurants.template.restContent.c
-    );
+    const cards = {
+      tag: 'div',
+      className: 'restContent',
+      c: data.map(
+        ({
+          mealPict,
+          discounts,
+          deliveryRate,
+          logo,
+          name,
+          restRating,
+          currentState,
+        }) => ({
+          tag: 'div',
+          className: 'restaurant',
+          c: [
+            {
+              tag: 'img',
+              src: `assets/meals/${mealPict}`,
+              className: 'mealPict',
+              alt: 'restaurant picture',
+            },
+            {
+              tag: 'span',
+              className: 'discounts',
+              textContent: `${discounts} % off`,
+            },
+            {
+              tag: 'span',
+              className: 'deliveryRate',
+              textContent: deliveryRate,
+            },
+            {
+              tag: 'img',
+              className: 'restLogo',
+              alt: 'Restaurant logo',
+              src: `assets/restaurants/${logo}`,
+            },
+            {
+              tag: 'span',
+              className: 'restName',
+              textContent: name,
+            },
+            {
+              tag: 'span',
+              className: 'ratings',
+              textContent: restRating,
+            },
+            {
+              tag: 'span',
+              className: currentState[0],
+              textContent: currentState[1],
+            },
+          ],
+        }),
+      ),
+    };
 
-    const cards = featuredRestaurants.template.restContent;
-
-    cards.c = {};
-
-    const { data } = featuredRestaurants;
-
-    Object.entries(data).map((card) => {
-      const [key, value] = card;
-
-      const template = structuredClone(restContent.restaurant);
-
-      const mealPict = template.c.mealPict.d;
-
-      mealPict.src = mealPict.src.concat(value.mealPict);
-
-      const discounts = template.c.discounts.d;
-
-      discounts.textContent = discounts.textContent.concat(value.discounts);
-
-      const deliveryRate = template.c.deliveryRate.d;
-
-      deliveryRate.textContent = value.deliveryRate;
-
-      const restLogo = template.c.restLogo.d;
-
-      restLogo.src = restLogo.src.concat(value.logo);
-
-      const restName = template.c.restName.d;
-
-      restName.textContent = value.name;
-
-      const ratings = template.c.ratings.d;
-
-      ratings.textContent = value.ratings;
-
-      const currentState = template.c.currentState.d;
-
-      const [className, textContent] = value.currentState;
-
-      currentState.className = className;
-
-      currentState.textContent = textContent;
-
-      cards.c[key] = template;
-
-      return cards.c[key];
-    });
-    const shell = {};
-
-    shell.featuredRestaurants = cards;
-
-    return shell;
+    return this.constructor.node(cards);
   }
 
   menuContent() {
-    const { menuContent } = this.settings;
+    const data = this.settings.menuContent;
 
-    const tempWrapper = menuContent.template.c;
+    const cards = {
+      tag: 'div',
+      className: 'menuContent',
+      c: data.map(({ img, name }) => ({
+        tag: 'div',
+        className: 'item',
+        c: {
+          mealPict: {
+            tag: 'img',
+            alt: 'Meal picture',
+            src: `assets/meals/foodCategories/${img}`,
+          },
+          name: {
+            tag: 'span',
+            textContent: name,
+          },
+        },
+      })),
+    };
 
-    const cards = menuContent.template;
-
-    cards.c = {};
-
-    const { data } = menuContent;
-
-    Object.entries(data).map((card) => {
-      const [key, value] = card;
-
-      const template = structuredClone(tempWrapper.item);
-
-      const mealPict = template.c.mealPict.d;
-
-      mealPict.src = mealPict.src.concat(value.img);
-
-      const name = template.c.name.d;
-
-      name.textContent = value.name;
-
-      cards.c[key] = template;
-
-      return cards.c[key];
-    });
-
-    const shell = {};
-
-    shell.menuContent = cards;
-
-    return shell;
+    return this.constructor.node(cards);
   }
 
   footerCities() {
-    const { cityList } = this.settings.footer;
+    const data = this.settings.footer.cityList;
 
-    const tempWrapper = cityList.template.c;
+    const cards = {
+      tag: 'div',
+      className: 'cityList',
+      c: data.map(([name, href]) => ({
+        tag: 'a',
+        target: '_self',
+        href: `https://${href}`,
+        textContent: name,
+      })),
+    };
 
-    const cards = cityList.template;
-
-    cards.c = {};
-
-    const { data } = cityList;
-
-    Object.entries(data).map((card) => {
-      const [name, href] = card;
-
-      const template = structuredClone(tempWrapper.link);
-
-      template.d.textContent = name;
-
-      template.d.href = template.d.href.concat(href);
-
-      cards.c[name] = template;
-
-      return cards.c[name];
-    });
-
-    const shell = {};
-
-    shell.cityList = cards;
-
-    return shell;
+    return this.constructor.node(cards);
   }
 
   footerMenu() {
-    const { navigation } = this.settings.footer;
+    const data = this.settings.footer.navigation;
 
-    const tempWrapper = navigation.template.c;
+    const cards = {
+      tag: 'div',
+      className: 'navigation',
+      c: data.map(({ title, nested }) => ({
+        tag: 'div',
+        className: 'division',
+        c: [
+          {
+            tag: 'h2',
+            textContent: title,
+          },
+          ...nested.map(({ title: name, href }) => ({
+            tag: 'a',
+            href: `https://${href}`,
+            target: '_self',
+            textContent: name,
+          })),
+        ],
+      })),
+    };
 
-    const cards = navigation.template;
-
-    cards.c = {};
-
-    const { data } = navigation;
-
-    Object.entries(data).map((card) => {
-      const [name, value] = card;
-
-      const { division } = structuredClone(tempWrapper);
-
-      const template = division.c;
-
-      template.title.d.textContent = name;
-
-      const subLevel = division;
-
-      subLevel.c = {};
-
-      subLevel.c[name] = template.title;
-
-      Object.entries(value).map((link) => {
-        const [text, href] = link;
-
-        const plate = structuredClone(template.link);
-
-        plate.d.textContent = text;
-
-        plate.d.href = plate.d.href.concat(href);
-
-        subLevel.c[text] = plate;
-
-        return subLevel.c[text];
-      });
-
-      cards.c[name] = division;
-
-      return cards.c[name];
-    });
-
-    const shell = {};
-
-    shell.navigation = cards;
-
-    return shell;
+    return this.constructor.node(cards);
   }
 
   footerNetworks() {
-    const { socialNetworks } = this.settings.footer;
+    const data = this.settings.footer.socialNetworks;
 
-    const tempWrapper = socialNetworks.template.c;
+    const cards = {
+      tag: 'div',
+      className: 'networks',
+      id: 'socialNetworks',
+      c: data.map((card) => {
+        const [className, href] = card;
+        return {
+          tag: 'a',
+          href: `https://${href}`,
+          c: {
+            networkPict: {
+              tag: 'div',
+              className: `socialIcons ${className}`,
+            },
+          },
+        };
+      }),
+    };
 
-    const cards = socialNetworks.template;
-
-    cards.c = {};
-
-    const { data } = socialNetworks;
-
-    Object.entries(data).map((card) => {
-      const [className, href] = card;
-
-      const template = structuredClone(tempWrapper.link);
-
-      template.d.href = template.d.href.concat(href);
-
-      const icon = template.c.networkPict.d;
-
-      icon.className = icon.className.concat(' ', className);
-
-      cards.c[className] = template;
-
-      return cards.c[className];
-    });
-
-    const shell = {};
-
-    shell.footerNetworks = cards;
-
-    return shell;
+    return this.constructor.node(cards);
   }
 }
 
