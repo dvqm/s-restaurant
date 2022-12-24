251d55c67987fe02f56c89c20bca67fd61af5c65
dvqm <10248779+dvqm@users.noreply.github.com>
2022-12-24 00:00:52 -0700

    refactored UiCreator. Deleted unnecessary functions.

--- src/UiCreator.js
+++ src/UiCreator.js
@@ -3,36 +3,6 @@ class UiCreator {
     return JSON.parse(JSON.stringify(obj));
   }
 
-  static ref(name, obj) {
-    let result;
-
-    const nameSearch = (prop) => {
-      if (name in prop) result = prop[name];
-
-      Object.values(prop).forEach((value) => {
-        if (typeof value === 'object') return nameSearch(value);
-        return result;
-      });
-    };
-    nameSearch(obj);
-
-    return result;
-  }
-
-  static preciseInsert(object, successor, key, value) {
-    const newObject = {};
-
-    Object.keys(object).forEach((property) => {
-      if (property === successor) {
-        newObject[key] = value;
-
-        newObject[property] = object[property];
-      } else newObject[property] = object[property];
-    });
-
-    return newObject;
-  }
-
   static node(data) {
     if (Object.keys(data).length === 0) return '';
 
@@ -42,29 +12,32 @@ class UiCreator {
 
     const parent = document.createElement(tag);
 
-    Object.entries(element).forEach((prop) => {
-      const [key, value] = prop;
+    Object.entries(element)
+      .forEach((prop) => {
+        const [key, value] = prop;
 
-      if (key !== 'tag' || key !== 'c' || key !== 's') parent[key] = value;
-      if (key.includes('data-')) parent.setAttribute(key, value);
-      if (key === 'required' && value === 'false') parent.removeAttribute(key);
-    });
+        if (key !== 'tag' || key !== 'c' || key !== 's') parent[key] = value;
+        if (key.includes('data-')) parent.setAttribute(key, value);
+        if (key === 'required' && value === 'false') parent.removeAttribute(key);
+        if (key === 'src') parent[key] = require(`../assets/${value}`);
+      });
 
     const blank = this.copy(data);
 
     if (blank.c) {
       const { c } = blank;
 
-      Object.values(c).forEach((node) => {
-        parent.append(this.node(node));
-      });
+      Object.values(c)
+        .forEach((node) => {
+          parent.append(this.node(node));
+        });
     }
 
     return parent;
   }
 
-  static composer(...handlers) {
-    return [...handlers].reduce((prev, next) => next(prev), {});
+  static composer(node, ...handlers) {
+    return [...handlers].reduce((prev, next) => next[0](prev, next[1]), node);
   }
 
   render(pointer, ...node) {
