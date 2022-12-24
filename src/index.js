fda918a440a049da0657a4e8d5157c5cbc83f836
dvqm <10248779+dvqm@users.noreply.github.com>
2022-12-24 00:07:05 -0700

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import basicHTML from './assets/json/basicHTML.json';
-import UiCreator from './UiCreator';
-import EventCreator from './EventCreator';
+import MainPage from './MainPage';
 
 // styles
 import './styles/index.scss';
@@ -13,10 +11,5 @@ import './styles/popularItems.scss';
 import './styles/promo.scss';
 import './styles/topNav.scss';
 
-const ui = new UiCreator();
-
-const events = new EventCreator();
-
-const mainPage = events.mainPage(ui.nodeCreate(basicHTML.wrapper));
-
-ui.render(document.body, mainPage);
+const mainPage = new MainPage();
+if (window.location.pathname !== '../assets/templates/underconstruct.html') mainPage.show();
