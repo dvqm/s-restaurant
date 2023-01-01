9be1bc2ce76eea7930aafd08c52d9bc557409341
dvqm <10248779+dvqm@users.noreply.github.com>
2022-12-31 22:31:16 -0700

    EventCreator Added pageRoute(), pagination() events

--- src/components/EventCreator.js
+++ src/components/EventCreator.js
@@ -100,6 +100,142 @@ class EventCreator {
 
     return node;
   }
+
+  pageRoute(node, sets) {
+    const {
+      route,
+      id,
+      eventType,
+    } = sets;
+
+    const btn = node.querySelector(`#${id}`);
+
+    btn.addEventListener(eventType, () => {
+      window.location.assign(route);
+    });
+
+    return node;
+  }
+
+  pagination(node, sets) {
+    const {
+      id,
+      nodesId,
+      nextBtnId,
+      prevBtnId,
+      eventType,
+      pageSize,
+    } = sets;
+
+    const btns = node.querySelectorAll(`.${id}`);
+
+    const nextBtn = node.querySelector(`#${nextBtnId}`);
+
+    const prevBtn = node.querySelector(`#${prevBtnId}`);
+
+    const restaurantNodes = node.querySelector(`#${nodesId}`);
+
+    const restaurantBackup = restaurantNodes.cloneNode(true);
+
+    const setFirstPage = (nodes, start, sizeOfPage) => {
+      const elements = Array.from(nodes.childNodes)
+        .slice(start, sizeOfPage);
+
+      nodes.innerHTML = '';
+
+      elements.forEach((element) => {
+        nodes.append(element);
+      });
+
+      btns[0].disabled = true;
+    };
+
+    const setElements = (nodes, content, ind, offset) => {
+      const elements = nodes.cloneNode(true);
+
+      for (let i = ind; i < ind + offset; i += 1) {
+        if (i >= nodes.childNodes.length) return;
+
+        content.append(elements.childNodes[ind]);
+      }
+    };
+
+    const enableBtns = () => {
+      btns.forEach((btn) => {
+        btn.disabled = false;
+      });
+    };
+
+    const getBtnId = (btn) => Number(btn.id.replace('startFrom', ''));
+
+    const checkBtnDisable = (btn, neighborBtn) => {
+      btn.disabled = !!neighborBtn.disabled;
+    };
+
+    const pagesToggle = (e) => {
+      let getIndexOfFirstChild;
+
+      for (let i = 0; i < btns.length; i += 1) {
+        const btn = btns[i];
+
+        if (btn.disabled) {
+          btn.disabled = false;
+
+          getIndexOfFirstChild = getBtnId(btn);
+
+          if (e.target.id === nextBtnId) {
+            btns[i + 1].disabled = true;
+          } else {
+            btns[i - 1].disabled = true;
+          }
+
+          if (e.target.id === nextBtnId && i + 1 === btns.length - 1) {
+            e.target.disabled = true;
+          } else if (e.target.id === prevBtnId && i - 1 === 0) e.target.disabled = true;
+
+          checkBtnDisable(prevBtn, btns[0]);
+
+          checkBtnDisable(nextBtn, btns[btns.length - 1]);
+
+          break;
+        }
+      }
+
+      const toggleIndex = e.target.id === nextBtnId
+        ? getIndexOfFirstChild + pageSize
+        : getIndexOfFirstChild - pageSize;
+
+      restaurantNodes.innerHTML = '';
+
+      setElements(restaurantBackup, restaurantNodes, toggleIndex, pageSize);
+    };
+
+    btns.forEach((btn, ind) => btn.addEventListener(eventType, (e) => {
+      const startItem = getBtnId(e.target);
+
+      enableBtns();
+
+      nextBtn.disabled = ind === btns.length - 1;
+
+      e.target.disabled = true;
+
+      restaurantNodes.innerHTML = '';
+
+      checkBtnDisable(prevBtn, btns[0]);
+
+      checkBtnDisable(nextBtn, btns[btns.length - 1]);
+
+      setElements(restaurantBackup, restaurantNodes, startItem, pageSize);
+    }));
+
+    nextBtn.addEventListener(eventType, pagesToggle);
+
+    prevBtn.addEventListener(eventType, pagesToggle);
+
+    setFirstPage(restaurantNodes, 0, pageSize);
+
+    return node;
+  }
 }
 
 export default EventCreator;
