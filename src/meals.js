5cb3605297228dddc6a76afb2b3e4bf73c4fc861
dvqm <10248779+dvqm@users.noreply.github.com>
2023-01-11 22:42:07 -0700

    meals.js modified to receive get params and other.

--- src/meals.js
+++ src/meals.js
@@ -18,19 +18,55 @@ class Meals extends UiCreator {
     super();
 
     this.content = new ContentCreator(data);
+
+    this.events = new EventCreator();
   }
 
   show() {
     const wrapper = this.content.wrapper();
 
+    const queryString = new URL(window.location.href).search;
+
+    const urlParams = new URLSearchParams(queryString);
+
+    const category = urlParams.get('menu');
+
+    const restId = urlParams.get('restid');
+
+    const filterMealsByRestaurant = (meals, id) => meals
+      .filter((meal) => meal.restaurant.find((rest) => rest.id === parseInt(id, 10)))
+      .map((meal) => {
+        meal.restaurant = meal.restaurant.filter((r) => r.id === parseInt(id, 10));
+        return meal;
+      });
+
+    let mealsToShow;
+
+    if (restId !== null && typeof restId === 'string') {
+      mealsToShow = filterMealsByRestaurant(data.menu, restId);
+    } else {
+      mealsToShow = data.menu;
+    }
+
+    const showMealsOrError = mealsToShow.length === 0
+      ? window.location.href = '/underconstruct.html?errmsg=emptyrest'
+      : this.content.meals(mealsToShow);
+
     this.render(
       wrapper,
       this.content.topNav(),
-      this.content.meals(),
+      showMealsOrError,
       this.content.footer(),
     );
+    const categorySwitch = [this.events.mealsToggle, {
+      startFrom: category,
+      wrapper: '.meals',
+      selector: '.ribbon',
+      eventType: 'click',
+    }];
 
-    this.render(document.body, wrapper);
+    const eventsChain = (node) => this.constructor.composer(node, categorySwitch);
+    this.render(document.body, eventsChain(wrapper));
   }
 }
 
