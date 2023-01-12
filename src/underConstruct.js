2c43d139486240c05d4947d9358499b61acb57ed
dvqm <10248779+dvqm@users.noreply.github.com>
2023-01-11 22:40:12 -0700

    underConstruct.js modified to receive get params and to return to the previous page in the history.

--- src/underConstruct.js
+++ src/underConstruct.js
@@ -5,14 +5,27 @@ const content = new ContentCreator();
 
 const ui = new UiCreator();
 
-ui.render(document.body, content.notFinished());
+const queryString = new URL(window.location.href).search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const errMsg = urlParams.get('errmsg');
+
+if (errMsg === null) {
+  ui.render(document.body, content.notFinished());
+} else if (errMsg === 'emptyrest') {
+  ui.render(
+    document.body,
+    content.notFinished('This restaurant did not have its menu'),
+  );
+}
 
 const btn = document.querySelector('#notFinished');
 
 const backdrop = document.querySelector('#backdrop');
 
 const cancel = () => {
-  window.location.assign('/');
+  window.history.back();
 };
 
 btn.addEventListener('click', cancel);
